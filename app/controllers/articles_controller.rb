16cbbc0e727192d8a4bd0030a2ff2b09c181fae8
Paul McCarthy <phantasos42@gmail.com>
2018-02-11 23:19:23 -0600

    edit article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,12 +6,26 @@ class ArticlesController < ApplicationController
   def new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def create
     @article = Article.new(article_params)
     @article.author = "Paul"
     @article.public = true
     @article.save
     redirect_to @article
+  end
+
+  def update
+    @article = Article.find(params[:id])
+   
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
   end
 
   def show
